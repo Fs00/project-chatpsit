3382e3c8aebf66430ef860d38c2e2291593cef02
Fs00 <francescosaltori@gmail.com>
2019-01-19 16:17:02 +0100

    Complete User class structure

--- chatpsit-server/src/chatpsit/server/User.java
+++ chatpsit-server/src/chatpsit/server/User.java
@@ -1,10 +1,40 @@
 package chatpsit.server;
 
-import java.net.Socket;
-import java.util.Date;
-
+/**
+ * Rappresenta l'insieme dei dati appartenenti ad un utente
+ */
 public class User
 {
     private String username;
-    private String displayName;
+    private String hashedPassword;
+    private String displayName;     // nome che verrà mostrato in chat
+    private boolean isAdmin;
+
+    public User(String username, String hashedPassword, String displayName, boolean isAdmin)
+    {
+        this.username = username;
+        this.hashedPassword = hashedPassword;
+        this.displayName = displayName;
+        this.isAdmin = isAdmin;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public String getHashedPassword()
+    {
+        return hashedPassword;
+    }
+
+    public String getDisplayName()
+    {
+        return displayName;
+    }
+
+    public boolean isAdmin()
+    {
+        return isAdmin;
+    }
 }
