e256c4b282c89c47d58a8dd9114d61a6a4527e71
Fs00 <francescosaltori@gmail.com>
2019-01-31 16:21:58 +0100

    Implement admin panel sidebar layout

--- chatpsit-admin-panel/src/chatpsit/adminpanel/MainWindowController.java
+++ chatpsit-admin-panel/src/chatpsit/adminpanel/MainWindowController.java
@@ -2,6 +2,8 @@ package chatpsit.adminpanel;
 
 import chatpsit.common.Message;
 import chatpsit.common.gui.IController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -10,6 +12,13 @@ public class MainWindowController implements IController
 {
     @FXML
     private BorderPane rootNode;
+
+    @FXML
+    public ObservableList<String> getSidebarEntries()
+    {
+        return FXCollections.observableArrayList("Chat globale",
+                "Ban utenti", "Segnalazioni", "Log del server");
+    }
 
     @FXML
     private void quitToLogin()
