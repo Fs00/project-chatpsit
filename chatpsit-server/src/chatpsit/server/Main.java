ecf8ed6b758187cb4ae775aad8a03f1c3f466468
Fs00 <francescosaltori@gmail.com>
2019-02-07 14:33:38 +0100

    Fix server shutdown hook not working

--- chatpsit-server/src/chatpsit/server/Main.java
+++ chatpsit-server/src/chatpsit/server/Main.java
@@ -7,7 +7,7 @@ public class Main
     public static void main(String[] args) throws Exception
     {
         Server serverInstance = new Server(ServerMode.Local);
-        new Thread(serverInstance).run();
+        new Thread(serverInstance).start();
 
         // Effettua le operazioni di chiusura del server quando viene richiesta la terminazione del processo
         Runtime.getRuntime().addShutdownHook(new Thread(serverInstance::shutdownServer));
